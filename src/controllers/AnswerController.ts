114361c06fe1906dae645f8de7c02adfd01396aa
Sylvn001 <juniorbaarros@gmail.com>
2022-04-13 22:20:16 -0300

    feat: Created NpsController and AnswerController to calculate nps.

--- src/controllers/AnswerController.ts
+++ src/controllers/AnswerController.ts
@@ -1,29 +1,28 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
+import { AppError } from "../errors/AppError";
 import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
 
 class AnswerController{
   async execute(request: Request, response: Response){
-    const {value} = request.params
-    const {u} = request.query
+    const { value } = request.params;
+    const { u } = request.query;
 
-    const surveyUserRepository = await getCustomRepository(SurveysUsersRepository)
+    const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
 
-    const surveyUser = await surveyUserRepository.findOne({
-      id: String(u)
-    })
+    const surveyUser = await surveysUsersRepository.findOne({
+      id: String(u),
+    });
 
-    if(!surveyUser){
-      return response.status(400).json({
-        error: "Survey User does not exist!"
-      })
-    }
+    if (!surveyUser)
+      throw new AppError("Survey User does not exists!");
 
-    surveyUser.value = Number(value)
+    surveyUser.value = Number(value);
 
-    await surveyUserRepository.save(surveyUser)
+    await surveysUsersRepository.save(surveyUser);
 
-    return response.json(surveyUser)
+    return response.json(surveyUser);
+
   }
 }
 
